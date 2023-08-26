a4092400409f2ee448dcd5e04d5ce989fd38c573
Tafshirul Tasib <tasib.tafshirul@gmail.com>
2023-08-26 22:22:31 +0600

    hello world

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
  
 
   return (
     <>
-      <div>
-      <h2>Hello world</h2>  
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ratione doloribus soluta eos explicabo maiores dicta, modi, adipisci natus illum ipsa veritatis? Aut obcaecati ea culpa eius maiores maxime! Eos reiciendis assumenda adipisci neque aut corrupti nisi vel odit animi voluptate dolor sequi, est deleniti cumque dicta quod pariatur. Incidunt ratione, corrupti at ducimus vitae autem impedit! Perspiciatis facilis culpa impedit est aperiam voluptate, enim necessitatibus sint corporis ab! Et expedita saepe dolorem culpa est laborum ex doloribus nemo enim molestias quas vero reprehenderit iure voluptatem, eveniet ea doloremque beatae voluptatum accusantium sunt adipisci, temporibus amet quae. Corrupti, odit vel.</p>
-      </div>
+     <Navbar/>
     </>
   )
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import './Navbar.css';
+
+
+const Navbar = () => {
+    // change background header
+  window.addEventListener("scroll", function () {
+    const header = this.document.querySelector(".header");
+    if (this.scrollY >= 80) header.classList.add("header-scroll");
+    else header.classList.remove("header-scroll");
+  });
+
+  // toggle Menu
+  const [toggle, showMenu] = useState(false);
+  const [activeNav, setActiveNav] = useState("#home");
+
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="/" className="nav-logo">
+          Rakib
+        </a>
+
+        <div className={toggle ? "nav-menu show-menu" : "nav-menu"}>
+          <ul className="nav-list grid">
+            <li className="nav-item">
+              <a
+                href="#home"
+                onClick={() => setActiveNav("#home")}
+                className={
+                  activeNav === "#home" ? "nav-link active-link" : "nav-link"
+                }
+              >
+                <i className="uil uil-estate nav-icon"></i> Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#about"
+                onClick={() => setActiveNav("#about")}
+                className={
+                  activeNav === "#about" ? "nav-link active-link" : "nav-link"
+                }
+              >
+                <i className="uil uil-user nav-icon"></i> About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#skills"
+                onClick={() => setActiveNav("#skills")}
+                className={
+                  activeNav === "#skills" ? "nav-link active-link" : "nav-link"
+                }
+              >
+                <i className="uil uil-file-alt nav-icon"></i> Skills
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#services"
+                onClick={() => setActiveNav("#services")}
+                className={
+                  activeNav === "#services"
+                    ? "nav-link active-link"
+                    : "nav-link"
+                }
+              >
+                <i className="uil uil-briefcase-alt nav-icon"></i> Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#qualification"
+                onClick={() => setActiveNav("#qualification")}
+                className={
+                  activeNav === "#qualification"
+                    ? "nav-link active-link"
+                    : "nav-link"
+                }
+              >
+                <i className="uil uil-graduation-cap nav-icon"></i> Qualification
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#projects"
+                onClick={() => setActiveNav("#projects")}
+                className={
+                  activeNav === "#projects"
+                    ? "nav-link active-link"
+                    : "nav-link"
+                }
+              >
+                <i className="uil uil-scenery nav-icon"></i> Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#contact"
+                onClick={() => setActiveNav("#contact")}
+                className={
+                  activeNav === "#contact" ? "nav-link active-link" : "nav-link"
+                }
+              >
+                <i className="uil uil-message nav-icon"></i> Contact
+              </a>
+            </li>
+          </ul>
+          <i
+            className="uil uil-times nav-close"
+            onClick={() => showMenu(!toggle)}
+          ></i>
+        </div>
+        <div className="nav-toggle" onClick={() => showMenu(!toggle)}>
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
